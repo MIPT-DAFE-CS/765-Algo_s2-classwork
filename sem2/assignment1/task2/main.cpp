cb20528a2253e42b8f86fb99f66cb20a13462736
yaishenka <gagarinovdaniil@gmail.com>
2018-04-12 00:36:21 +0300

    fix

--- sem2/assignment1/task2/main.cpp
+++ sem2/assignment1/task2/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 
-#include "IGraph.h"
+
+struct IGraph {
+    virtual ~IGraph() {}
+    
+    virtual void AddEdge(int from, int to) = 0;
+    virtual int VerticesCount() const = 0;
+    
+    virtual void GetNext(int vertex, std::vector<int>& vertices) const = 0;
+    virtual void GetPrev(int vertex, std::vector<int>& vertices) const = 0;
+};
 
 class CListGraph : public IGraph {
 private:
