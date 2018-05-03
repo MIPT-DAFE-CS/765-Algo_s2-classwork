395b27341a8572705762e0214c0ce9a75a3b6298
yaishenka <gagarinovdaniil@gmail.com>
2018-05-03 17:01:04 +0300

    not solved

--- sem2/assignment3/task4/main.cpp
+++ sem2/assignment3/task4/main.cpp
@@ -1,58 +1,186 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <map>
+#include <queue>
 using namespace std;
-enum Type = {inLine, inTemplate};
-class Vertex
-{
-public:
-    char _name;
-    short _number;
-    int _q_index;
-    Type type;
-}
+enum Type {inLine, inTemplate, cons};
 
-class Graph
-{
-public:
+struct Edge {
+    int from, to;
+    int capacity;
+    int flow;
+};
+
+class Graph {
+private:
+    std::vector<std::vector<Edge>> _adjacency;
     size_t _size;
-    std::vector<std::vector<short>>  _graph;
-    vector<Vertex> _vertexes;
-    Graph (string s, string p)
-    {
-        _vertexes.push_back (Vertex ("0", 0, -1, inLine));
-        for (int i{0}; i < s.size(); i++) {
-            auto ch = s[i];
-            if (ch == "?") {
-                _size++;
-                _vertexes.push_back (Vertex ('?', _size - 1, i, inLine));
-            }
-        }
-        for (int i{0}; i < p.size(); i++) {
-            auto ch = p[i];
-            if (ch == "?") {
-                _size++;
-                _vertexes.push_back (Vertex ('?', _size - 1, i, inTemplate));
-            }
-        }
-        _vertexes.push_back (Vertex ("1", _size++, -1, inLine));
-        _graph.resize(_size);
-        for (auto v : _graph)
-            v.resize(_size, 0);
 
+public:
+    Graph (size_t s) {
+      _adjacency.resize(s, std::vector<Edge>(s, Edge{0,0,0,0}));
+      _size = s;
     }
-    void fill_weights(strings s, )
-    {
 
+    void increaseEdgeCapacity(int i, int j) {
+      _adjacency[i][j].capacity++;
+      _adjacency[j][i].capacity++;
     }
+
+    void increaseEdgeFlow(int i, int j, int flow) {
+      _adjacency[i][j].capacity+= flow;
+      _adjacency[j][i].capacity+= flow;
+    }
+
+    int possibleFlow(int i, int j) {
+        return _adjacency[i][j].capacity - _adjacency[i][j].flow;
+    }
+
+    size_t size() {return _size; }
+
+    const Edge & operator()(int i, int j) const {
+        return _adjacency[i][j];
+    }
+};
+
+bool bfs(Graph & graph, int from, int to, vector<int> & parent)
+{
+	queue <int> queue;
+    vector <bool> visited(graph.size(), false);
+	queue.push(from);
+	visited[from] = true;
+	while (!queue.empty())
+	{
+		int cur = queue.front();
+		visited[cur] = true;
+		queue.pop();
+		for (int i(0); i < graph.size(); ++i)
+		{
+            auto edge = graph(cur, i);
+            if (visited[edge.to] || edge.capacity == edge.flow) continue;
+			queue.push(edge.to);
+			parent[edge.to] = i;
+			if (edge.to == to) return true;
+		}
+	}
+	return false;
 }
 
-string solver ()
+int find(Graph & graph, int from, int to, vector<int> &parent) {
+    int min_edge{numeric_limits<int>::max()};
+	int cur{to};
+	while (cur != from) {
+        int possibleFlow = graph.possibleFlow(parent[cur], cur);
+		if (possibleFlow <  min_edge)
+			min_edge = possibleFlow;
+		cur = parent[cur];
+	}
+    return min_edge;
+}
 
+void push_flow(Graph & graph, int from, int to, vector<int> &parent, int min_edge) {
+	int cur(to);
+	while (cur != from) {
+        graph.increaseEdgeFlow(min_edge, from, to);
+		cur = parent[cur];
+	}
+}
 
-int main()
+int ford_falkerson(Graph & graph, int from, int to) {
+	vector<int> parent(graph.size());
+	int min_edge, ans(0);
+	bool is_way(false);
+	while (true)
+	{
+		is_way = bfs(graph, from, to, parent);
+		if (!is_way) break;
+		min_edge = find(graph, from, to, parent);
+		push_flow(graph, from, to, parent, min_edge);
+		ans += min_edge;
+	}
+	return ans;
+}
+
+int solver (string & s, string & t) {
+    int qInS(0);
+    int qInT(0);
+    int index{2};
+    int distance{0};
+    std::map <int, std::pair<Type, int>> conteiner1;
+    std::map <std::pair<Type, int>, int> conteiner2;
+    for ( int i{0}; i<s.size(); ++i)  {
+        if (s[i] == '?') {
+            qInS++;
+            conteiner1[index] = std::make_pair (Type::inLine, i);
+            conteiner2[std::make_pair (Type::inLine, i)]=index++;
+        }
+    }
+    for ( int i{0}; i<s.size(); ++i)  {
+        if (s[i] == '?') {
+            qInT++;
+            conteiner1[index] = std::make_pair (Type::inTemplate, i);
+            conteiner2[std::make_pair (Type::inTemplate, i)]=index++;
+        }
+    }
+    Graph _graph(qInS + qInT + 2);
+
+    for (int i{0}; i + t.size() < s.size(); ++i) {
+        string tmp = s.substr(i, t.size());
+        for (int j{0}; j < t.size(); ++j) {
+            if (tmp[j]!='?' && t[j]!='?' && tmp[j]!=t[j]) distance++;
+            switch (tmp[j]) {
+                case '?':
+                    switch (t[j]) {
+                        case '?':
+                            _graph.increaseEdgeCapacity(conteiner2[std::make_pair(Type::inLine,i+j)], conteiner2[std::make_pair(Type::inLine,j)]);
+                            break;
+                        case '0':
+                            _graph.increaseEdgeCapacity(conteiner2[std::make_pair(Type::inLine,i+j)], 0);
+                            break;
+                        case '1':
+                            _graph.increaseEdgeCapacity(conteiner2[std::make_pair(Type::inLine,i+j)], 1);
+                            break;
+                    }
+                    break;
+            }
+            switch (t[j]) {
+                case '?':
+                    switch (tmp[j]) {
+                        case '0':
+                            _graph.increaseEdgeCapacity(conteiner2[std::make_pair(Type::inTemplate,j)], 0);
+                            break;
+                        case '1':
+                            _graph.increaseEdgeCapacity(conteiner2[std::make_pair(Type::inTemplate,j)], 1);
+                            break;
+                    }
+                    break;
+            }
+        }
+    }
+
+    distance += ford_falkerson(_graph, 0, 1);
+    for (int i(2); i < _graph.size(); ++i) {
+        if (_graph.possibleFlow(i, 0)) {
+            if (conteiner1[i].first == Type::inLine) {
+                s[conteiner1[i].second] = '0';
+            } else {
+                t[conteiner1[i].second] = '0';
+            }
+        } else {
+            if (conteiner1[i].first == Type::inLine) {
+                s[conteiner1[i].second] = '1';
+            } else {
+                t[conteiner1[i].second] = '1';
+            }
+        }
+    }
+    return distance;
+}
+
+int main ()
 {
-    string s, p;
-    std::cin >> s >> p;
-
-
+     string s,t;
+     cin >> s >> t;
+     cout << solver(s,t) << endl << s << endl << t;
 }
